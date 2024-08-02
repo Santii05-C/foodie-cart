49ca80a8622dc54e93676269517f7544c8373d18
Santii05-C <santi.c.pgm@gmail.com>
2024-08-02 10:20:49 -0300

    last commit

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 function Header() {
-  return <div>//36:51</div>;
+  return (
+    <div>
+      <Image src="/logo.png" alt="logo" width={200} height={200} />
+
+      <div className="flex border p-2 rounded-lg bg-gray-200 w-96">
+        <input type="text" className="bg-transparent w-full outline-none" />
+        <Search />
+      </div>
+
+      <div>
+        <Button>Login</Button>
+        <Button>Sign Up</Button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
