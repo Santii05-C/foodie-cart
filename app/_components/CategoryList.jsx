f243cd478d96556968648ab720b4b8e0b3ec9100
Santii05-C <santi.c.pgm@gmail.com>
2024-08-03 12:45:05 -0300

    last commit

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -1,6 +1,7 @@
 "use client";
-import React, { useEffect, useState, useState } from "react";
+import React, { useEffect, useState } from "react";
 import GlobalApi from "../_utils/GlobalApi";
+import Image from "next/image";
 
 function CategoryList() {
   const [categoryList, setCategoryList] = useState([]);
@@ -13,7 +14,28 @@ function CategoryList() {
       setCategoryList(resp.categories);
     });
   };
-  return <div>CategoryList</div>;
+  //1:10
+  return (
+    <div>
+      <div className="flex gap-4">
+        {categoryList &&
+          categoryList.map((category, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center gap-2 border p-3 rounded-xl min-w-28"
+            >
+              <Image
+                src={category.icon?.url}
+                alt={category.name}
+                width={40}
+                height={40}
+              />
+              <h2>{category.name}</h2>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default CategoryList;
