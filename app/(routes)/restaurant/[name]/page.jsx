db75f8999e217d0decb07d24b3a675137e1dcde2
Santii05-C <santi.c.pgm@gmail.com>
2024-08-07 16:22:30 -0300

    last commit

--- app/(routes)/restaurant/[name]/page.jsx
+++ app/(routes)/restaurant/[name]/page.jsx
@@ -1,19 +1,26 @@
 "use client";
 import GlobalApi from "@/app/_utils/GlobalApi";
 import { usePathname } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Intro from "../_components/Intro";
 
 function RestaurantDetails() {
   const param = usePathname();
+  const [restaurant, setRestaurant] = useState([]);
   useEffect(() => {
-    console.log(param);
+    GetBusinessDetails(param.split("/")[2]);
   }, []);
-  const GetBusinessDetails = () => {
-    GlobalApi.GetBusinessDetail().then((resp) => {
-      console.log(resp);
+  const GetBusinessDetails = (restroSlug) => {
+    GlobalApi.GetBusinessDetail(restroSlug).then((resp) => {
+      setRestaurant(resp.restaurant);
     });
   };
-  return <div>RestaurantDetails</div>;
+  return (
+    <div>
+      <Intro restaurant={restaurant} />
+    </div>
+  );
 }
 
 export default RestaurantDetails;
+//2:12
