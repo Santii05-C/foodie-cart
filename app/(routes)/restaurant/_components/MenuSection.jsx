999425904a98bc68fa33522a8bae02ac8fb6834d
Santii05-C <santi.c.pgm@gmail.com>
2024-08-08 10:58:11 -0300

    last commit

--- app/(routes)/restaurant/_components/MenuSection.jsx
+++ app/(routes)/restaurant/_components/MenuSection.jsx
@@ -1,8 +1,26 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 function MenuSection({ restaurant }) {
-  return <div>MenuSection</div>;
+  const filterMenu = () => {};
+  return (
+    <div>
+      <div className="grid grid-cols-4 mt-2">
+        <div className="hidden md:flex flex-col mr-10 gap-2">
+          {restaurant?.menu?.map((item, index) => (
+            <Button
+              variant="ghost"
+              className="flex justify-start"
+              onClick={() => filterMenu(item.category)}
+            >
+              {item.category}
+            </Button>
+          ))}
+        </div>
+        <div className="col-span-3">Menu List</div>
+      </div>
+    </div>
+  );
 }
 
 export default MenuSection;
-//2:23
