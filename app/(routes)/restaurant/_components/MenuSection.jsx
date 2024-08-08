13323e5b5c71fa52719b92f89a217db3b867c6a2
Santii05-C <santi.c.pgm@gmail.com>
2024-08-08 12:57:35 -0300

    last commit

--- app/(routes)/restaurant/_components/MenuSection.jsx
+++ app/(routes)/restaurant/_components/MenuSection.jsx
@@ -1,26 +1,38 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 function MenuSection({ restaurant }) {
-  const filterMenu = () => {};
+  const [menuItemList, setMenuItemList] = useState([]);
+
+  const filterMenu = (category) => {
+    const result = restaurant?.menu?.filter(
+      (item) => item.category == category
+    );
+    setMenuItemList(result[0]);
+  };
+
   return (
-    <div>
-      <div className="grid grid-cols-4 mt-2">
-        <div className="hidden md:flex flex-col mr-10 gap-2">
-          {restaurant?.menu?.map((item, index) => (
-            <Button
-              variant="ghost"
-              className="flex justify-start"
-              onClick={() => filterMenu(item.category)}
-            >
-              {item.category}
-            </Button>
-          ))}
-        </div>
-        <div className="col-span-3">Menu List</div>
+    <div className="grid grid-cols-4 mt-2">
+      <div className="hidden md:flex flex-col mr-10 gap-2">
+        {restaurant?.menu?.map((item, index) => (
+          <Button
+            variant="ghost"
+            key={index}
+            className="flex justify-start"
+            onClick={() => filterMenu(item.category)}
+          >
+            {item.category}
+          </Button>
+        ))}
+      </div>
+      <div className="col-span-3">
+        <h2 className="font-extrabold text-lg">{menuItemList.category}</h2>
+        <div></div>
       </div>
     </div>
   );
 }
 
 export default MenuSection;
+//2:30
